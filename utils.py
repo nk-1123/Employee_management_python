78498ce205aa2dfe64be625c3fb0fb3d7c980d1d
Nikita <nk01119888@gmail.com>
2023-10-12 10:42:33 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,9 +1,4 @@
-from datetime import date
 import random
 
 def generateEmpID(): #to generate emp id
     return random.randrange(100, 199)
-
-def calculate_age(dob):
-    today = date.today()
-    return today.year - dob.year - ((today.month, today.day) < (dob.month, dob.day))
