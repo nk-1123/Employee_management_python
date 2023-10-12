091ee550e16eee17e5c940eef1ceeaa0e7dbd0e5
Nikita <nk01119888@gmail.com>
2023-10-12 10:34:05 +0530

    Updated employee.py

--- employee.py
+++ employee.py
@@ -21,7 +21,24 @@ class Employee:
         self.performance_rating = performance_rating
     
     def display(self):
-        print(self.eid, self.name, self.email, self.sal, self.age, self.gender, self.contact, self.address, self.city, self.dob, self.doj, self.dept, self.designation, self.pan, self.adhaar, self.performance_rating)
+        print(
+            "Eid: ", self.eid,
+            "Name: ", self.name,
+            "Email: ", self.email,
+            "Salary: ", self.sal, 
+            "Age: ", self.age,
+            "Gender: ", self.gender,
+            "Contact: ", self.contact,
+            "Address: ", self.address,
+            "City: ", self.city,
+            "DOB: ", self.dob,
+            "DOJ: ", self.doj,
+            "Department: ", self.dept,
+            "Designation: ", self.designation,
+            "PAN: ", self.pan,
+            "Adhaar: ", self.adhaar,
+            "Performance rating: ", self.performance_rating
+            )
 
 EmployeeList = []
     
@@ -31,41 +48,44 @@ while True:
     print("3. Press 3 to delete the record ")
     print("4. Press 4 to update the record ")
     print("5. Press 5 to search the record ")
-    print("6. Press 6 to display the employee details with highest/lowest salary as per the input ") #merge both of these
-    # print("7. Press 7 to display the employee details with lowest salary ") #isko hata
-    print("7. Press 7 to display all Employees details from a specific department ")  #iska code likh
-    print("8. Press 8 to display the best employees name from the entered department ") #yeh bhi
+    print("6. Press 6 to display the employee details with highest/lowest salary as per the input ") 
+    print("7. Press 7 to display all Employees details from a specific department ")  
+    print("8. Press 8 to display the best employees name from the entered department ") 
     print("9. Press 9 to exit ")
 
     choice = int(input("Enter your choice: "))
     if choice == 1:
         eid = generateEmpID()
-        name = input("Enter employee name: ")
         while True:
+            name = input("Enter employee name: ")
             if validateName(name):
                 break
             else:
                 print("Invalid name entered.")
+                print("")
         while True:
             email = input("Enter employee email: ")
             if(validateEmail(email)):
                 break
             else:
                 print("Invalid email entered.")
+                print("")
         while True:
             salary = int(input("Enter employee salary: "))
             if(validateSalary(salary)):
                 break
             else:
                 print("Invalid salary entered.")
+                print("")
 
-                # age will automate
+                
         while True:
             age = int(input("Enter employee age: "))
             if(validateAge(age)):
                 break
             else:
                 print("Invalid age entered.")
+                print("")
 
         while True:
             gender = input("Enter 'M' for male, 'F' for female or 'O' for others: ")
@@ -73,9 +93,10 @@ while True:
                 break
             else:
                 print("Invalid gender entered.")
+                print("")
 
         while True:
-            contact = int(input("Enter mobile number: "))
+            contact = input("Enter mobile number: ")
             if(validateContactNo(contact)):
                 break
             else:
@@ -89,6 +110,7 @@ while True:
                 break
             else:
                 print("Invalid city entered.")
+                print("")
 
         while True:
             dob = input("Enter date of birth: ")
@@ -96,42 +118,49 @@ while True:
                 break
             else:
                 print("Invalid date entered.")
+                print("")
         while True:
             doj = input("Enter date of joining: ")
             if(validateDOJ(doj)):
                 break
             else:
                 print("Invalid date entered.")
+                print("")
         while True:
             dept = input("Enter the department: ")
-            if(validateDeptName(dept, EmployeeList)): 
+            if(validateDeptName(dept)): 
                 break
             else:
                 print("Invalid department entered.")
+                print("")
         while True:
             designation = input("Enter the designation: ")
-            if(validateDesignation(designation, EmployeeList)): 
+            if(validateDesignation(designation)): 
                 break
             else:
                 print("Invalid designation entered.")
+                print("")
         while True:
             pan = input("Enter the PAN ID: ")
             if(validatePan(pan)):
                 break
             else:
                 print("Invalid PAN ID entered.")
+                print("")
         while True:
-            aadhaar = int(input("Enter the Aadhaar number: "))
+            aadhaar = input("Enter the Aadhaar number: ")
             if(validateAadhaar(aadhaar)):
                 break
             else:
                 print("Invalid Aadhaar number entered.")
+                print("")
         while True:
-            performance_rating = int(input("Enter the rating out of 5: "))
+            performance_rating = int(input("Enter the rating out of 10: "))
             if(validatePerformance(performance_rating)):
                 break
             else:
                 print("Invalid rating inserted.")
+                print("")
 
         print("Record inserted")
         obj = Employee(eid, name, email, salary, age, gender, contact, address, city, dob, doj, dept, designation, pan, aadhaar, performance_rating)
@@ -146,118 +175,124 @@ while True:
     elif choice == 3:
         inputID = int(input("Enter the employee ID for which the record has to be deleted: "))
         for i in EmployeeList:
-            if i.id == inputID:
+            if i.eid == inputID:
                 EmployeeList.remove(i)
+
     elif choice == 4:
         while True:
-            print("1. Enter 1 to update name of employee")
-            print("2. Enter 2 to update address of employee")
-            print("3. Enter 3 to update DOB of employee")
-            print("4. Enter 4 to update salary of employee")
-            print("5. Enter 5 to exit")
-            updateChoice = int(input("Enter the option: "))
-            inputID = int(input("Enter the id:"))
-            if choice == 1:
+            print("1. Enter A to update name of employee")
+            print("2. Enter B to update address of employee")
+            print("3. Enter C to update DOB of employee")
+            print("4. Enter D to update salary of employee")
+            print("5. Enter E to go back")
+            updateChoice = input("Enter the option: ")
+
+            if updateChoice == 'A':
+                inputID = int(input("Enter the id:"))
                 for i in EmployeeList:
-                    if i.id == inputID:
+                    if i.eid == inputID:
                         updatedName = input("Enter the new name: ")
-                        EmployeeList[i].name = updatedName
-            elif choice == 2:
+                        i.name = updatedName
+                        print("Name successfully updated.")
+                        print("")
+                        
+            elif updateChoice == 'B':
+                inputID = int(input("Enter the id:"))
                 for i in EmployeeList:
-                    if i.id == inputID:
+                    if i.eid == inputID:
                         updatedAddress = input("Enter the new address: ")
-                        EmployeeList[i].address = updatedAddress
-            elif choice == 3:
+                        i.address = updatedAddress
+                        print("Address successfully updated.")
+                        print("")
+                        
+            elif updateChoice == 'C':
+                inputID = int(input("Enter the id:"))
                 for i in EmployeeList:
-                    if i.id == inputID:
+                    if i.eid == inputID:
                         updatedDOB = input("Enter the new date of birth: ")
-                        EmployeeList[i].dob = updatedDOB
-            elif choice == 4:
-                print("1. Enter 1 to update the salary of specific employee by employee id.")
-                print("2. Enter 2 to update the salary of all employees working ins specific department.")
-                print("3. Enter 3 to update the salary of all employees")
-                print("4. Enter 4 to exit")
-                updateChoice = int(input("Enter the option: "))
-                if choice == 1:
-                    inputID = int(input("Enter the employee id: "))
-                    for i in EmployeeList:
-                        if i.id == inputID:
-                            inputSalary = int(input("Enter the salary: "))
-                            EmployeeList[i].salary = inputSalary
-                elif choice == 2:
-                    inputDept = input("Enter the department: ")
-                    for i in EmployeeList:
-                        if i.dept == inputDept:
-                            inputSalary = int(input("Enter the salary: "))
-                            EmployeeList[i].salary = inputSalary
-                elif choice == 3:
-                    inputSalary = int(input("Enter the salary: "))
-                    for i in EmployeeList:
-                        EmployeeList[i].salary = inputSalary
-                elif choice == 4:
-                    break
-                else:
-                    print("Invalid choice entered.")
-            elif choice == 5:
+                        i.dob = updatedDOB
+                        print("DOB successfully updated.")
+                        print("")
+                        
+            elif updateChoice == 'D':
+                inputID = int(input("Enter the id:"))
+                for i in EmployeeList:
+                    if i.eid == inputID:
+                        inputSalary = int(input("Enter the salary: "))
+                        i.salary = inputSalary
+                        print("Salary successfully updated.")
+                        print("")
+                        
+            elif updateChoice == 'E':
                 break
             else:
                 print("Invalid choice")
+                print('')
+
     elif choice == 5:
-        print("1. Enter 1 to search the details of specific employee by employee id")
-        print("2. Enter 2 to search by employee name")
-        print("3. Enter 3 to search by Department name")
-        print("4. Enter 4 to exit")
-        searchChoice = int(input("Enter the choice: "))
-        if searchChoice == 1:
+        print("1. Enter A to search the details of a employee by employee id")
+        print("2. Enter B to search by employee name")
+        print("3. Enter C to search by Department name")
+        print("4. Enter D to exit")
+        searchChoice = input("Enter the choice: ")
+        if searchChoice == 'A':
             inputID = int(input("Enter the employee ID: "))
             for i in EmployeeList:
-                if i.id == inputID:
+                if i.eid == inputID:
                     i.display()
                 else:
                     print("Employee ID is invalid.")
-        elif searchChoice == 2:
+                    print("")
+        elif searchChoice == 'B':
             inputName = input("Enter the employee name: ")
             for i in EmployeeList:
                 if i.name == inputName:
                     i.display()
                 else:
                     print("Employee name is invalid.")
-        elif searchChoice == 3:
+                    print("")
+        elif searchChoice == 'C':
             inputDept = input("Enter the employee department: ")
             for i in EmployeeList:
                 if i.dept == inputDept:
                     i.display()
                 else:
                     print("Employee department is invalid.")
-        elif searchChoice == 4:
+                    print("")
+        elif searchChoice == 'D':
             break
         else:
             print("Invalid search choice.")
+            print("")
+
     elif choice == 6:
         dept = input("Enter department name: ")
         print("Type 'min' to display the minimun salary: ")
         print("Type 'max' to display the minimun salary: ")
-        sal = input("Enter your choice: ")
+        sal = int(input("Enter your choice: "))
         SalaryList = []
 
         if sal == 'min':
             for i in EmployeeList:
-                SalaryList.append(i.sal)
+                SalaryList.append(i.sal) 
                 minSal = min(SalaryList)
             print("Details of employees with minimum salary:")
             for i in EmployeeList:
                 if i.sal == minSal:
                     i.display()
+                    print('')
         elif sal == 'max':
             for i in EmployeeList:
-                SalaryList.append(i.sal)
+                SalaryList.append(i.sal) 
                 maxSal = max(SalaryList)
             print("Details of employees with maximum salary:")
             for i in EmployeeList:
                 if i.sal == maxSal:
                     i.display()
+                    print('')
         else:
             print("invalid choice entered.")
+            print('')
 
     elif choice == 7:
         #print all dept name
@@ -267,8 +302,12 @@ while True:
         for i in EmployeeList:
             if i.dept == dept:
                 EmpDeptList.append(i.name)
-        for i in EmpDeptList:
-            print(EmpDeptList[i])
+        if len(EmpDeptList) != 0:
+            for i in EmpDeptList:
+                print(i)
+        else:
+            print("NO employees present in this department")
+        print('')
 
 
     elif choice == 8:
@@ -285,13 +324,16 @@ while True:
         for i in EmployeeList:
             if i.dept == dept and i.performance_rating == max_rating:
                 BestEmpList.append(i.name)
-        
-        for i in BestEmpList:
-            print(BestEmpList[i])
+        if(len(BestEmpList) != 0):
+            for i in BestEmpList:
+                print(i)
+        else:
+            print("No Employees present in this department")
+            print("")
 
-    
     elif choice == 9:
         print("Program terminated successfully.")
         break
+    
     else:
         print("Invalid choice entered.")
